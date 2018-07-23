8eac42ee9ba40fd26e97be2a40c0d11b3b975b01
GSMSpringApps <gsmohanty88@gmail.com>
2018-07-24 03:36:03 +0530

    All Settings like gitignore & application.properties etc. are Saved

---
+++ src/main/java/com/gsm/spring/SpringBootAppGithubDemoApplication.java
@@ -0,0 +1,12 @@
+package com.gsm.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppGithubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppGithubDemoApplication.class, args);
+	}
+}
